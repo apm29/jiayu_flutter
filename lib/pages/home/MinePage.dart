302b3f1c7d81d097e8fd65263a7e0e220f734143
apm29 <yjw999wow@163.com>
2020-07-22 17:35:25 +0800

    MinePage.dart [WIP]

--- lib/pages/home/MinePage.dart
+++ lib/pages/home/MinePage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jiayu_flutter/components/scroll/draggable_upper_drawer.dart';
+import 'package:jiayu_flutter/pages/style/styles.dart';
 
 ///
 /// author : apm29
@@ -9,6 +9,70 @@ import 'package:jiayu_flutter/components/scroll/draggable_upper_drawer.dart';
 class MinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: DraggableUpperDrawerWidget());
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [mainColor, mainSecondaryColor.withAlpha(0x77)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter
+                    ),
+                  ),
+                  height: 200,
+                ),
+                Positioned(
+                  top: 100,
+                  left: 16,
+                  right: 16,
+                  child: Material(
+                    elevation: 12,
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    child: Container(
+                      padding: EdgeInsets.all(12),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundColor: Colors.grey[400],
+                                child: Icon(
+                                  Icons.person_outline,
+                                  size: 22,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Expanded(child: Text('未登录'))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('我的收藏'),
+              subtitle: Text('个人收藏的产品在这里'),
+            ),
+            ListTile(
+              leading: Icon(Icons.face),
+              title: Text('咨询客服'),
+              subtitle: Text('在线询问价格'),
+            ),
+            // ListTiles++
+          ]),
+        ),
+      ],
+    );
   }
 }
