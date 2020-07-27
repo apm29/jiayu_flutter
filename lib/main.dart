7bcfaf7ec982ce765bdd026effb05e7e9ed7edce
apm29 <yjw999wow@163.com>
2020-07-27 10:57:32 +0800

    移除sentry

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:jiayu_flutter/application/Application.dart';
 import 'package:jiayu_flutter/pages/SplashPage.dart';
 import 'package:jiayu_flutter/store/ReduxApp.dart';
-import 'package:sentry/sentry.dart';
 
-final SentryClient sentry = new SentryClient(dsn: "https://d374b2ab1e6c41619a3bef3fdde4133b@o424313.ingest.sentry.io/5356009");
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,11 +24,9 @@ void main() async {
 
 customerReport(
     {FlutterErrorDetails detail, Object error, StackTrace stackTrace}) {
-  if (detail != null) {
-    print(detail);
-  } else {
-    print(error);
-    print(stackTrace);
-  }
-  sentry.captureException(exception: error,stackTrace: stackTrace);
+    print(detail??"NO DETAIL");
+    print(detail?.stack??"NO DETAIL STACK");
+    print(detail?.exception??"NO DETAIL EXCEPTION");
+    print(error??"NO ERROR");
+    print(stackTrace??"NO STACKTRACE");
 }
